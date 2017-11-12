7a9626a19a9925485d72eb6685f52cc66637c090
Alexandre Page <alexandre.page@epitech.eu>
2017-11-12 15:08:29 +0100

    basic test for CRUD operations

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -3,8 +3,6 @@ class Api::V1::AppsController < ApplicationController
 
 	def create
 		@app = App.create(app_params)
-		puts app_params.inspect
-		puts @app.inspect
 		render json: @app
 	end
 
--- spec/controllers/api/v1/apps_controller_spec.rb
+++ spec/controllers/api/v1/apps_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::AppsController, type: :controller do
 
 	let!(:app) { FactoryGirl.create(:app) }
+	let!(:second_app) { FactoryGirl.create(:app) }
 
 	describe "POST #create" do
 
@@ -13,6 +14,28 @@ RSpec.describe Api::V1::AppsController, type: :controller do
 			expect(response.body).to be_json_eql(app.to_json)
 			expect(response).to be_success
 			expect(response).to have_http_status(200)
+		end
+	end
+
+	describe "PUT #update" do
+
+		it "should update a resource" do
+			put :update, { id: app.id, name: second_app.name, price: second_app.price, rating: second_app.rating, link: second_app.link, image: second_app.image,
+				genres: second_app.genres }
+
+			expect(response.body).to be_json_eql(second_app.to_json)
+			expect(response).to be_success
+		end
+	end
+
+	describe "DELETE #destroy" do
+
+		it "removes successfully a resource" do
+			delete :destroy, { :id => second_app.id }
+			parsed_response = JSON.parse(response.body)
+
+			expect(response).to have_http_status(200)
+			expect(parsed_response['message']).to eq("Done")
 		end
 	end
 
