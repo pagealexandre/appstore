08a3fc6e9ebe944eef783e8c7fe73d4160e4443b
Alexandre Page <alexandre.page@epitech.eu>
2017-11-13 14:06:07 +0100

    App.rb

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,17 +3,17 @@ class App < ActiveRecord::Base
 	include AlgoliaSearch
 
 	algoliasearch :per_environment => true do
-    	attribute :name, :rating, :price, :genres, :link, :image
-    	searchableAttributes ['name', 'rating', 'price']
+    	attribute :name, :rating, :price, :genres, :image
+    	searchableAttributes ['name']
 
     	attributesForFaceting [:genres]
 
-	    add_replica 'App_price_asc', per_environment: true do
-	      customRanking ['asc(price)']
+	    add_replica 'App_price_asc', inherit: true, per_environment: true do
+	      customRanking ['asc(price)', 'desc(rating)']
 	    end
 
 	    add_replica 'App_price_desc', inherit: true, per_environment: true do
-	      customRanking ['desc(price)']
-	    end	    
+	      customRanking ['desc(price)', 'desc(rating)']
+	    end
   	end
 end
